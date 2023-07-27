3fc6ab8848dd55060d120d466034f995a3cd1a17
Omree Kapon <51881832+ok-bar@users.noreply.github.com>
2023-07-27 16:20:58 +0300

    Update json_reader.py

--- json_reader.py
+++ json_reader.py
@@ -22,8 +22,6 @@ def load_images(data,images_folder):
       else:
                 file_name=data.iloc[i,:]['ID']
                 img=data.iloc[i,:]['Labeled Data']
-                
-
                 try:
                   im = Image.open(requests.get(img, stream=True).raw if img.startswith('http') else img)
                   width, height = im.size  # image size
@@ -31,14 +29,15 @@ def load_images(data,images_folder):
                   a=data.iloc[i,:]['Label']['objects']
                   listy=[]
                   count+=1
-                
                   for j in range(len(a)):
+                    cls = a[j]['value'] 
+                    if cls != 'building':
+                       pass
+                    else:
                           top, left, h, w = a[j]['bbox'].values() # top, left, height, width
                           xywh = [(left + w / 2) / width, (top + h / 2) / height, w / width, h / height]  # xywh normalized
                           path=save_to_folder+'/'+f'{file_name}.txt'
                           cls = a[j]['value']  # class name
-                          if cls not in names:
-                                names.append(cls)
                           obj=0,*xywh
                           listy.append(obj)
                   with open(path, 'w') as f:
@@ -46,10 +45,6 @@ def load_images(data,images_folder):
                       f.write(' '.join(str(s) for s in t) + '\n')
                 except PIL.UnidentifiedImageError:
                       print(img)
-                
     print(f'number of images is {count}')
-    return names
-                    
 
-names=load_images(data,path)              
-print(f'classes: {names}')
+names=load_images(data,path)  
