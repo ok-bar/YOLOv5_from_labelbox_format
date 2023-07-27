c171469c2ae6c420e3cdf924337b087cc8f9df72
Omree Kapon <51881832+ok-bar@users.noreply.github.com>
2023-07-27 16:26:18 +0300

    Update add_background.py

--- add_background.py
+++ add_background.py
@@ -1,9 +1,24 @@
-def background_for_yolo(x_train_dir,background_path):
+start=int(len(os.listdir(x_train_dir))*0.1)
+med=start+int(len(os.listdir(x_val_dir))*0.1)
+end=med+int(len(os.listdir(x_test_dir))*0.1)
 
-  background_images=os.listdir(background_path)
-  for i in tqdm(range(len(background_images))):
-      img=background_images[i]
-      img=background_path+img
-      new_img=x_train_dir+'/'+background_images[i]
-      shutil.copyfile(img, new_img)
 
+for i in tqdm(range(0,start)):
+    img=background_images[i]
+    img='/content/drive/MyDrive/active_learn_data/train/2/'+img
+    new_img=x_train_dir+'/'+background_images[i]
+    shutil.copyfile(img, new_img)
+
+
+
+for i in tqdm(range(start,med)):
+    img=background_images[i]
+    img='/content/drive/MyDrive/active_learn_data/train/2/'+img
+    new_img=x_val_dir+'/'+background_images[i]
+    shutil.copyfile(img, new_img)
+
+for i in tqdm(range(med,end)):
+    img=background_images[i]
+    img='/content/drive/MyDrive/active_learn_data/train/2/'+img
+    new_img=x_test_dir+'/'+background_images[i]
+    shutil.copyfile(img, new_img)
